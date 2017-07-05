863499f6bcba4c90774395c876307e527a445e82
阿拉斯加的狗 <1281433811@qq.com>
2017-07-05 15:55:55 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-testPodsLib_Tests/Pods-testPodsLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testPodsLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_testPodsLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/testPodsLib/testPodsLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testPodsLib : NSObject
+@end
+@implementation PodsDummy_testPodsLib
+@end
